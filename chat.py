d5b41adfbc75cc5ce601b0a5fc47ce0c9fb4c0b4
Jayaharish R <93772259+jr1010@users.noreply.github.com>
2023-12-30 13:55:18 +0530

    Add files via upload

--- chat.py
+++ chat.py
@@ -5,10 +5,10 @@ from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-with open('intents.json','r') as f:
+with open('intents.json', 'r') as f:
     intents = json.load(f)
 
-FILE="data.pth"
+FILE = "data.pth"
 data = torch.load(FILE)
 
 input_size = data["input_size"]
@@ -21,9 +21,36 @@ model_state = data["model_state"]
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
-bot_name = "Hariesh"
-print("Let's chat! type 'quit' to exit")
-while True:
+bot_name = "ChatBot!"
+
+
+def get_response(sentence):
+    a1 = sentence +"\n"
+    if sentence == 'quit':
+        return "Bye"
+    sentence = tokenize(sentence)
+    X = bag_of_words(sentence, all_words)
+    X = X.reshape(1, X.shape[0])
+    X = torch.from_numpy(X)
+
+    output = model(X)
+    _, predicted = torch.max(output, dim=1)
+    tag = tags[predicted.item()]
+
+    probs = torch.softmax(output, dim=1)
+    prob = probs[0][predicted.item()]
+
+    if prob.item() > 0.75:
+        for intent in intents["intents"]:
+            if tag == intent["tag"]:
+                return (f'{random.choice(intent["responses"])}')
+    else:
+        return ("Sorry, I don't understand")
+        with open("Unrecognized.txt", 'a') as f:
+            f.write(a1)
+
+
+"""while True:
     sentence = input('You:')
     a1=sentence+"\n"
     if sentence == "quit":
@@ -49,7 +76,7 @@ while True:
     else:
         print("I dont understand")
         with open("Unrecognized.txt",'a') as f:
-            f.write(a1)
+            f.write(a1)"""
 
 
 
